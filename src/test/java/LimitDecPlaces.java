78b50cc36c4a4eeb3c0a085391eee11994c6c376
Ksenia Bevzyuk <ksunechka-92@yandex.ru>
2025-03-06 02:26:25 +0300

    modiff pom.xml

---
+++ src/test/java/LimitDecPlaces.java
@@ -0,0 +1,10 @@
+public class LimitDecPlaces {
+    public static float roundToPlaces (float value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (float) tmp / factor;
+    }
+}
